622ba73c0a3aa7d0b3b09bddf038fc2a2c998b59
smmhatami <hatamik@gmail.com>
2023-11-22 18:43:06 +0330

    shipping tests developed

--- test/ShippingTest.java
+++ test/ShippingTest.java
@@ -25,4 +25,52 @@ public class ShippingTest {
         assertEquals(plan.calcPrice(1.5), 5.25);
     }
 
+
+    @Test
+    public void testCreateShipping() {
+        PackState state;
+        Pack pack;
+        ShippingPlan plan;
+        Shipping shipping;
+        state = new InTransit();
+        pack = new Pack("book", 300, state);
+        plan = new StandardPlan();
+        shipping = new Shipping(pack, plan);
+        assertEquals(shipping.getPlan(), plan);
+        assertEquals(shipping.getPack(), pack);
+    }
+
+
+    @Test
+    public void testShippingSetPlan() {
+        PackState state;
+        Pack pack;
+        ShippingPlan plan;
+        Shipping shipping;
+        state = new InTransit();
+        pack = new Pack("book", 300, state);
+        plan = new StandardPlan();
+        shipping = new Shipping(pack, plan);
+        plan = new ExpressPlan();
+        shipping.setPlan(plan);
+        assertEquals(shipping.getPlan(), plan);
+    }
+
+
+    @Test
+    public void testShippingCalcPrice() {
+        PackState state;
+        Pack pack;
+        ShippingPlan plan;
+        Shipping shipping;
+        state = new InTransit();
+        pack = new Pack("book", 300, state);
+        plan = new StandardPlan();
+        shipping = new Shipping(pack, plan);
+        assertEquals(shipping.calcPrice(), 750);
+        plan = new ExpressPlan();
+        shipping.setPlan(plan);
+        assertEquals(shipping.calcPrice(), 1225);
+    }
+
 }
