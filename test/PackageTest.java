8f65dca0b5ad2b8e91598d5100c0400f22de1585
smmhatami <hatamik@gmail.com>
2023-11-22 18:17:45 +0330

    package tests developed

--- test/PackageTest.java
+++ test/PackageTest.java
@@ -50,4 +50,50 @@ public class PackageTest {
         state.deliver();
         assertEquals(state.tellState(), "delivered");
     }
+
+    @Test
+    public void testCreatePackage() {
+        PackageState state = new InTransit();
+        Package package = new Package("paper", 100, state);
+        assertEquals(package.tellState(), "in transit");
+        assertEquals(package.getName(), "paper");
+        assertEquals(package.getWeight(), 100);
+
+        state = new Delivered();
+        package = new Package("book", 300, state);
+        assertEquals(package.tellState(), "delivered");
+        assertEquals(package.getName(), "book");
+        assertEquals(package.getWeight(), 300);
+    }
+
+    @Test
+    public void testPackageChangeState() {
+        PackageState inTransit = new InTransit();
+        PackageState delievred = new Delivered();
+        Package package = new Package("paper", 100, inTransit);
+        package.changeState(delievred);
+        assertEquals(package.tellState(), "delivered");
+        package.changeState(inTransit);
+        assertEquals(package.tellState(), "in transit");
+    }
+
+    @Test
+    public void testPackageDeliver() {
+        PackageState state;
+        Package package;
+
+        state  = new InTransit();
+        package = new Package("book", 300, state);
+        state.setPackage(package);
+        package.deliver();
+        assertEquals(package.tellState(), "delivered");
+
+        state = new Delivered();
+        package = new Package("book", 300, state);
+        state.setPackage(package);
+        package.deliver();
+        assertEquals(package.tellState(), "delivered");
+    }
+
+
 }
